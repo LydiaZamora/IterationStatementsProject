d74e16fcb67c872f8c27067aae56d0f699e85ce1
Lydia Zamora <lydiazamorayoga@gmail.com>
2024-06-19 11:46:00 -0700

    Finished project

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -7,30 +7,7 @@ namespace IterationStatements
     {
         static void Main(string[] args)
         {
-                //TODO - Read each comment and complete its instruction
-                // like the example below
-
-                //Create a List called "numbers" - DONE!
                 
-            
-            //-----START HERE------------------------------------------
-            
-
-            // Create a for loop - this will print the numbers in reverse order - from 200 to 1
-                // in your initializer set the value of i to 199
-                // in your conditional, as long as i is less than or equal to the amount of items in "numbers" - use (numbers.Count)
-                // AND as long as i is greater than or equal to 0
-                // Decrement i by 1
-
-            //start for loop here
-            {
-                // place numbers[i] inside of the Console.WriteLine() method
-            }
-
-            //------------End of exercise
-
-
-
             var numbers = new List<int>();
 
             var myNumber = 0;
